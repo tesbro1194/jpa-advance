d6b5488aa0be3be583932332c60e2b9a6a7be3da
tesbro1194 <tes1194@naver.com>
2024-10-14 14:01:11 +0900

    중간 테이블 Order 테스트

--- src/main/java/com/sparta/jpaadvance/entity/Order.java
+++ src/main/java/com/sparta/jpaadvance/entity/Order.java
@@ -4,19 +4,20 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Getter
 @Setter
-@Table(name = "users")
-public class User {
+@Table(name = "orders")
+public class Order {  // 외래키의 주인
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
 
-    @OneToMany(mappedBy = "user")
-    private List<Order> orderList = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "food_id")
+    private Food food;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
